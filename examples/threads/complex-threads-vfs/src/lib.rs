2cad0ba57739e01d9996e84d4991b57628395ff9
oligami <nziq53@gmail.com>
2026-04-26 21:37:57 +0900

    feat: implement PollOneoff for handling clock subscriptions in the polling module

--- examples/threads/complex-threads-vfs/src/lib.rs
+++ examples/threads/complex-threads-vfs/src/lib.rs
@@ -98,3 +98,100 @@ mod fs {
 
     plug_fs!(&VIRTUAL_FILE_SYSTEM, anonymous, self);
 }
+
+mod poll {
+
+    struct WaitPoll;
+
+    impl PollOneoff for WaitPoll {
+        #[inline(never)]
+        fn poll_oneoff<Wasm: WasmAccess>(
+            subscriptions_ptr: *const wasi_virt_layer::__private::wasip1::Subscription,
+            ret_event_ptr: *mut wasi_virt_layer::__private::wasip1::Event,
+            nsubscriptions: wasi_virt_layer::__private::wasip1::Size,
+            ret_stored_events_ptr: *mut wasi_virt_layer::__private::wasip1::Size,
+        ) -> wasi_virt_layer::__private::wasip1::Errno {
+            use wasi_virt_layer::__private::wasip1::*;
+
+            if nsubscriptions == 0 {
+                return ERRNO_INVAL;
+            }
+
+            // TODO: For now, we only support a single subscription just to be enough for wasi-libc's
+            // clock_nanosleep.
+            if nsubscriptions > 1 {
+                return ERRNO_NOTSUP;
+            }
+
+            let (userdata, event_type, timeout, precision, flags) = unsafe {
+                let base_ptr = subscriptions_ptr as *const u8;
+
+                let userdata = Wasm::load_le::<u64>(base_ptr as *const u64);
+                let event_type = Wasm::load_le::<u8>(base_ptr.add(8) as *const u8);
+                // let clock_id = Wasm::load_le::<u32>(base_ptr.add(16) as *const u32);
+                let timeout = Wasm::load_le::<Timestamp>(base_ptr.add(24) as *const Timestamp);
+                let precision = Wasm::load_le::<Timestamp>(base_ptr.add(32) as *const Timestamp);
+                let flags = Wasm::load_le::<Subclockflags>(base_ptr.add(40) as *const Subclockflags);
+
+                (userdata, event_type, timeout, precision, flags)
+            };
+
+            // TODO: For now, we only support clock subscriptions.
+            if event_type != EVENTTYPE_CLOCK.raw() {
+                return ERRNO_NOTSUP;
+            }
+
+            fn get_now() -> Timestamp {
+                let sys_time = std::time::SystemTime::now();
+
+                let nano = sys_time
+                    .duration_since(std::time::SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_nanos() as Timestamp;
+
+                nano
+            }
+
+            // Perform the wait
+            let end_time = if (flags & SUBCLOCKFLAGS_SUBSCRIPTION_CLOCK_ABSTIME) != 0 {
+                timeout
+            } else {
+                get_now().saturating_add(timeout)
+            }
+            .saturating_sub(precision);
+
+            loop {
+                std::thread::yield_now();
+
+                let now = get_now();
+
+                // println!("### Current time: {}", now);
+
+                if now >= end_time {
+                    break;
+                }
+            }
+
+            // Write an event to the out buffer
+            let event = Event {
+                userdata: userdata,
+                error: ERRNO_SUCCESS,
+                type_: EVENTTYPE_CLOCK,
+                fd_readwrite: EventFdReadwrite {
+                    nbytes: 0,
+                    flags: 0,
+                },
+            };
+
+            Wasm::store_le(ret_event_ptr, event);
+            Wasm::store_le(ret_stored_events_ptr, 1);
+
+            ERRNO_SUCCESS
+        }
+    }
+
+    use wasi_virt_layer::poll::PollOneoff;
+
+    use super::*;
+    plug_poll!(WaitPoll, anonymous, self);
+}
